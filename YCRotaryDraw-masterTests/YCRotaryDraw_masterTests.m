0f7a9a06784d47f8360f12744b3415485c5d09b1
ZongAng123 <zongang15@yahoo.com>
2017-02-24 13:29:21 +0800

    Initial Commit

---
+++ YCRotaryDraw-masterTests/YCRotaryDraw_masterTests.m
@@ -0,0 +1,39 @@
+//
+//  YCRotaryDraw_masterTests.m
+//  YCRotaryDraw-masterTests
+//
+//  Created by 纵昂 on 2017/2/24.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YCRotaryDraw_masterTests : XCTestCase
+
+@end
+
+@implementation YCRotaryDraw_masterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
